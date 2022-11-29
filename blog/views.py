5ba589c6b1b820b07cd6dcb155884588f0dc146d
RYgithub1 <ryo.rec.otani@gmail.com>
2022-11-29 16:44:14 +0100

    Divided app2 from app1.

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
-from django.http import HttpResponse
 
 
 
@@ -46,8 +45,3 @@ def post_edit(request, pk):
   else:
       form = PostForm(instance=post)
   return render(request, 'blog/post_edit.html', {'form': form})
-
-
-def firstviewfunction(request):
-  print(dir(request))
-  return HttpResponse('RESPONSED')
--- mys/urls.py
+++ mys/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('adminadmin/', admin.site.urls),
 
     path('', include('blog.urls')),
+    path('book', include('book.urls')),
 ]
