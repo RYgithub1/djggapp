99cae5295bf965190829da82e21b66685d564875
RYgithub1 <ryo.rec.otani@gmail.com>
2022-12-16 22:09:38 +0100

    Added views

--- helloworld/views.py
+++ helloworld/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+
+def helloworldfunction(request):
+  returnedobject = HttpResponse('<h1>HELLO WORLD</h1>')
+  return returnedobject
