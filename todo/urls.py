fb6b80970a4a6a68060299c6d27024da98b71fca
RYgithub1 <ryo.rec.otani@gmail.com>
2022-12-28 22:46:58 +0100

    Added CreateView.

--- todo/urls.py
+++ todo/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.TodoList.as_view(), name='TodoList'), # CLASS -> as_view()
     path('detail/<int:pk>/', views.TodoDetail.as_view(), name='TodoDetail'),
+    path('create', views.TodoCreate.as_view(), name='TodoCreate'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
 from .models import TodoModel
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
 
 '''
 TODO:
@@ -16,3 +17,13 @@ class TodoList(ListView):
 class TodoDetail(DetailView):
   template_name = 'todo/detail.html'
   model = TodoModel
+
+
+class TodoCreate(CreateView):
+  template_name = 'todo/create.html'
+  model = TodoModel
+  fields = ('title', 'memo', 'priority', 'duedate')
+
+  ## NEED: Transfer to an URL(name=TodoList) after CREATE.
+  ## After SAVE, transfer slowly(lazily).
+  success_url = reverse_lazy('TodoList')
