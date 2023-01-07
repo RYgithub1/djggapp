b57dd3a4cff793ea5f4d6165c822c76b965b2ace
RYgithub1 <ryo.rec.otani@gmail.com>
2023-01-07 23:10:08 +0100

    Fixed sign-up function to save user data.

--- board/views.py
+++ board/views.py
@@ -1,24 +1,21 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.models import User
 
 
 
-def qwerty(request):
-  return HttpResponse('RESPONSED')
-
-
 def signupfunc(request):
-  ## object_list = User.objects.all()
+  # object_list = User.objects.all()
   # object = User.objects.get(username='djgguser')
+
   if request.method == 'POST':
-    # print(request.POST) # FORM INPUT DATA -> MAKE AN USER.
+    print("request.method == 'POST'")
+    ## FORM INPUT DATA -> MAKE AN USER.
+    ## Need name-value of InputTag in FormTag to get each data.
+    print(request.POST)
     username = request.POST['username']
     password = request.POST['password']
     user = User.objects.create_user(username, '', password)
-
   else:
-    print('-------------- NOT POST')
-
+    print("request.method == 'GET'")
 
   return render(request, 'board/signup.html', {'some': 11100})
