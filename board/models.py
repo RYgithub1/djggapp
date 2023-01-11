84435cacd6b4cf7e5ecaa3d4befc63d40bc88814
RYgithub1 <ryo.rec.otani@gmail.com>
2023-01-11 12:18:59 +0100

    Made a snsboard models.

--- board/models.py
+++ board/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Board(models.Model):
+  snsTitle = models.CharField(max_length=100)
+  snsContent = models.TextField()
+  snsAuthor = models.CharField(max_length=50)
+  snsImage = models.ImageField(upload_to='')
+  snsGood = models.IntegerField()
+  snsRead = models.IntegerField()
+  personsWhoRead = models.TextField()
+
+  def __str__(self):
+    return self.snsTitle
