4f71cd1debb808b91019aef00232519debf04d0b
RYgithub1 <ryo.rec.otani@gmail.com>
2023-02-25 22:46:29 +0100

    Added about-info

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.myapp_index, name='myapp_index'),
+    path('', views.myapp_index, name='myapp_index'), # 127.0.0.1:8000/myapp
+    path('about/', views.myapp_about, name='myapp_about'),
+    path('info/', views.myapp_info, name='myapp_info'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -7,3 +7,14 @@ def myapp_index(request):
   context = {'name':'Yade'}
   #return HttpResponse("Hello, world")
   return render(request, 'myapp/index.html', context)
+
+
+
+def myapp_about(request):
+  context = {}
+  return render(request, 'myapp/about.html', context)
+
+
+def myapp_info(request):
+  context = {}
+  return render(request, 'myapp/info.html', context)
