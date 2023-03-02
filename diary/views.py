2eb2ad7ceff5f4977edb2f29bc6db3122fc736b6
RYgithub1 <ryo.rec.otani@gmail.com>
2023-03-02 23:54:10 +0100

    fixed add method

--- diary/views.py
+++ diary/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import DayCreateForm
 
 
@@ -10,8 +10,15 @@ def diary_index(request):
 
 
 def diary_add(request):
+  form = DayCreateForm(request.POST or None)  # if no POST -> None
+
+  if request.method == 'POST' and form.is_valid():
+    form.save()
+    # POST -> redirect stop duprication
+    return redirect('diary_index')
+
   context = {
-    'form': DayCreateForm()
+    'form': form
   }
   return render(request, 'diary/day_form.html', context)
 
