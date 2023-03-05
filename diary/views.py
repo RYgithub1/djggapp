4a0f2c5a81ac0e8ef364986b701cef5c62fa69e9
RYgithub1 <ryo.rec.otani@gmail.com>
2023-03-05 23:58:21 +0100

    Added delete

--- diary/views.py
+++ diary/views.py
@@ -37,7 +37,21 @@ def diary_update(request, pk):
     return redirect(diary_index)
 
   context = {
-      'form':form
+      'form':form,
     }
   return render(request, 'diary/day_form.html', context)
+
+
+
+def diary_delete(request, pk):
+  day = get_object_or_404(Day, pk=pk)
+
+  if request.method == 'POST':
+    day.delete()
+    return redirect(diary_index)
+
+  context = {
+      'day':day,
+    }
+  return render(request, 'diary/day_confirm_delete.html', context)
 
