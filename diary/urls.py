dcc58c23f0644d095a749058bcd92f72c0f1d694
RYgithub1 <ryo.rec.otani@gmail.com>
2023-03-08 22:27:38 +0100

    Added some link-to-admin

--- diary/urls.py
+++ diary/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.diary_index, name='diary_index'), # 127.0.0.1:8000/diary
-    path('add/', views.diary_add, name='diary_add'),
-    path('update/<int:pk>', views.diary_update, name='diary_update'),
-    path('delete/<int:pk>', views.diary_delete, name='diary_delete'),
+    # path('add/', views.diary_add, name='diary_add'),
+    # path('update/<int:pk>', views.diary_update, name='diary_update'),
+    # path('delete/<int:pk>', views.diary_delete, name='diary_delete'),
     path('detail/<int:pk>', views.diary_detail, name='diary_detail'),
 ]
