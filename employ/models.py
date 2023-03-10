a2a5c3ca218681c0035c7751bb264656e333ccab
RYgithub1 <ryo.rec.otani@gmail.com>
2023-03-10 22:31:06 +0100

    Made models and admin

--- employ/models.py
+++ employ/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+
+class Department(models.Model):
+  name = models.CharField('Department Name', max_length=22)
+  created_at = models.DateTimeField('Date', default=timezone.now)
+
+  def __str__(self):
+    return self.name
+
+
+
+class Employee(models.Model):
+  first_name = models.CharField('first name', max_length=20)
+  last_name = models.CharField('last name', max_length=20)
+  email = models.EmailField('mail address', blank=True)
+  department = models.ForeignKey(
+    Department,
+    verbose_name = 'department',
+    on_delete = models.PROTECT,
+  )
+  created_at = models.DateTimeField('Date', default=timezone.now)
+
+  def __str__(self):
+    return '{0}{1}{2}'.format(self.last_name, self.first_name, self.department)
