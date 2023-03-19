ed330876f71056c5895d10346cb60d981ec3acc7
RYgithub1 <ryo.rec.otani@gmail.com>
2023-03-19 22:30:37 +0100

    Made models

--- journal/models.py
+++ journal/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+
+class JournalCategory(models.Model):
+  name = models.CharField('CATEGORY NAME', max_length=255)
+  created_date = models.DateTimeField('CREATED DATE', default=timezone.now)
+
+  def __str__(self):
+    return self.name
+
+
+
+class JournalPost(models.Model):
+  title = models.CharField('TITLE', max_length=255)
+  text = models.TextField('TEXT')
+  created_date = models.DateTimeField('CREATED DATE', default=timezone.now)
+
+  category = models.ForeignKey(
+    JournalCategory,
+    verbose_name = 'CATEGORY',
+    on_delete = models.PROTECT,
+  )
+
+  def __str__(self):
+    return self.title
