3a3341dd8e73a1a01f13194f3f2902ce9cf4b8b9
RYgithub1 <ryo.rec.otani@gmail.com>
2023-03-20 21:05:43 +0100

    Added admin and journalpost

--- journal/admin.py
+++ journal/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import JournalCategory, JournalPost
 
-# Register your models here.
+
+
+admin.site.register(JournalCategory)
+admin.site.register(JournalPost)
--- journal/views.py
+++ journal/views.py
@@ -1,8 +1,13 @@
 # from django.shortcuts import render
 from django.views import generic
+from .models import JournalPost
 
 
 
-class IndexView(generic.TemplateView):
-  template_name = 'journal/post_list.html'
+# class IndexView(generic.TemplateView):
+#   template_name = 'journal/post_list.html'
+class IndexView(generic.ListView):
+  model = JournalPost
 
+  def get_queryset(self):
+    return JournalPost.objects.order_by('-created_date')
