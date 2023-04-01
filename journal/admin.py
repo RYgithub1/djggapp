80f3eb626caa61bdb8cc696f1e5936fed317de9a
RYgithub1 <ryo.rec.otani@gmail.com>
2023-04-01 22:27:31 +0200

    Made JournalComment model

--- journal/admin.py
+++ journal/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import JournalCategory, JournalPost
+from .models import JournalCategory, JournalPost, JournalComment
 
 
 
 admin.site.register(JournalCategory)
 admin.site.register(JournalPost)
+admin.site.register(JournalComment)
--- journal/models.py
+++ journal/models.py
@@ -16,7 +16,6 @@ class JournalPost(models.Model):
   title = models.CharField('TITLE', max_length=255)
   text = models.TextField('TEXT')
   created_date = models.DateTimeField('CREATED DATE', default=timezone.now)
-
   category = models.ForeignKey(
     JournalCategory,
     verbose_name = 'CATEGORY',
@@ -25,3 +24,18 @@ class JournalPost(models.Model):
 
   def __str__(self):
     return self.title
+
+
+
+class JournalComment(models.Model):
+  name = models.CharField('NAME', max_length=30)
+  comment = models.TextField('COMMENT')
+  created_at = models.DateTimeField('CREATED DATE', default=timezone.now)
+  post = models.ForeignKey(
+    JournalPost,
+    verbose_name = 'ARTICLE',
+    on_delete = models.PROTECT,
+  )
+
+  def __str__(self):
+    return self.comment[:10]
