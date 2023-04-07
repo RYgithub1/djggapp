2547a41969cf61275bf8397f44f6431a196c1940
RYgithub1 <ryo.rec.otani@gmail.com>
2023-04-07 22:25:24 +0200

    Fixed urls

--- videos/urls.py
+++ videos/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.videoslist, name='videoslist'),
+    path('', views.VideoIndexView.as_view, name='videoindexview'),
+    path('upload/', views.VideoCreateView.as_view, name='videocreateview'),
+    path('play/<int:pk>/', views.VideoPlayView.as_view, name='videoplayview'),
 ]
