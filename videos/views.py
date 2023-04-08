d368f70a37f86bf322bd9b9e4bdcd7adf9ca6ff9
RYgithub1 <ryo.rec.otani@gmail.com>
2023-04-08 22:35:03 +0200

    Added views

--- videos/views.py
+++ videos/views.py
@@ -1,6 +1,21 @@
-# from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+from .forms import VideoCreateForm
+from .models import Video
 
 
 
-def videoslist():
-  pass
+class VideoIndexView(generic.ListView):
+  model = Video
+
+
+
+class VideoCreateView(generic.CreateView):
+  model = Video
+  form_class = VideoCreateForm
+  success_url = reverse_lazy('videoindexview')
+
+
+
+class VideoPlayView(generic.DetailView):
+  model = Video
