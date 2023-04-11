39ae8d7eb2bb19c508122770893ec1d82979c87f
RYgithub1 <ryo.rec.otani@gmail.com>
2023-04-11 22:44:47 +0200

    Added basehtml

--- videos/urls.py
+++ videos/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.VideoIndexView.as_view, name='videoindexview'),
-    path('upload/', views.VideoCreateView.as_view, name='videocreateview'),
-    path('play/<int:pk>/', views.VideoPlayView.as_view, name='videoplayview'),
+    path('', views.VideoIndexView.as_view(), name='videoindexview'),
+    path('upload/', views.VideoCreateView.as_view(), name='videocreateview'),
+    path('play/<int:pk>/', views.VideoPlayView.as_view(), name='videoplayview'),
 ]
