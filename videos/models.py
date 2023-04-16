da2c026e28d796d9061cbdfa5a62149fa55239b0
RYgithub1 <ryo.rec.otani@gmail.com>
2023-04-16 22:46:52 +0200

    Fixed admin and models

--- videos/models.py
+++ videos/models.py
@@ -5,7 +5,10 @@ from django.db import models
 class Video(models.Model):
   title = models.CharField('TITLE', max_length=255)
   description = models.TextField('DESCRIPTION (or EMPTY)', blank=True)
-  thumbnail = models.ImageField('STHUMBNAIL (or EMPTY)', upload_to='thumbnails/', null=True, blank=True)
+  thumbnail = models.ImageField('THUMBNAIL (or EMPTY)', upload_to='thumbnails/', null=True, blank=True)
   upload = models.ImageField('FILE', upload_to='uploads/%Y/%m/%d/')
   created_at = models.DateTimeField('CREATED DATE', auto_now_add=True)
   updated_at = models.DateTimeField('UPDATED DATE', auto_now=True)
+
+  def __str__(self):
+    return self.title
