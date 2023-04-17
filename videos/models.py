f519a2ea6816de673b75ed038b2c43c4156183ee
RYgithub1 <ryo.rec.otani@gmail.com>
2023-04-17 21:50:31 +0200

    Fixed models

--- videos/models.py
+++ videos/models.py
@@ -6,7 +6,7 @@ class Video(models.Model):
   title = models.CharField('TITLE', max_length=255)
   description = models.TextField('DESCRIPTION (or EMPTY)', blank=True)
   thumbnail = models.ImageField('THUMBNAIL (or EMPTY)', upload_to='thumbnails/', null=True, blank=True)
-  upload = models.ImageField('FILE', upload_to='uploads/%Y/%m/%d/')
+  upload = models.FileField('FILE', upload_to='uploads/%Y/%m/%d/')
   created_at = models.DateTimeField('CREATED DATE', auto_now_add=True)
   updated_at = models.DateTimeField('UPDATED DATE', auto_now=True)
 
