289b42be55c7bd7a5ed114da57c3c8bc1375896f
RYgithub1 <ryo.rec.otani@gmail.com>
2023-04-22 23:07:22 +0200

    Added views

---
+++ mnist/lib.py
@@ -0,0 +1,54 @@
+import pickle
+import numpy as np
+from PIL import Image
+from sklearn import datasets, svm
+from sklearn.model_selection import train_test_split
+
+
+
+def read():
+    with open('mnist.pickle', 'rb') as file:
+        clf = pickle.load(file)
+    return clf
+
+
+
+def create_and_save():
+    mnist = datasets.fetch_openml('mnist_784', data_home='image/')
+    X = mnist.data / 255
+    y = mnist.target
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, train_size=1000, test_size=300
+    )
+
+    clf = svm.SVC()
+    clf.fit(X_train, y_train)
+
+    with open('mnist.pickle', 'wb') as file:
+        pickle.dump(clf, file)
+    return clf
+
+
+
+try:
+    clf = read()
+except FileNotFoundError:
+    clf = create_and_save()
+
+
+
+def predict(img_array):
+    result = clf.predict(img_array)
+    return str(int(result[0]))
+
+
+
+if __name__ == '__main__':
+    for i in range(0, 10):
+        file_name = '{}.png'.format(i)
+        img = Image.open(file_name)
+        img = np.asarray(img) / 255
+        img_array = img.reshape(1, 784)
+        result = predict(img_array)
+        print(result)
--- mnist/views.py
+++ mnist/views.py
@@ -1,3 +1,47 @@
+import base64
+from io import BytesIO
 from django.shortcuts import render
+from django.views import generic
+from .forms import ImageUploadForm
+from .lib import predict
+import numpy as np
+from PIL import Image
 
-# Create your views here.
+
+
+class UploadView(generic.FormView):
+    template_name = 'mnist/upload.html'
+    form_class = ImageUploadForm
+
+    def form_valid(self, form):
+        file = form.cleaned_data['file']
+
+        # Resize to 28*28 and monokuro
+        img = Image.open(file).resize((28, 28)).convert('L')
+
+        # Convert the data
+        img_array = np.asarray(img) / 255
+        img_array = img_array.reshape(1, 784)
+
+        context = {
+            'result': predict(img_array),
+        }
+        return render(self.request, 'mnist/result.html', context)
+
+
+
+class PaintView(generic.TemplateView):
+    template_name = 'mnist/paint.html'
+
+    def post(self, request):
+        base_64_string = request.POST['img-src'].replace(
+            'data:image/png;base64,', '')
+
+        file = BytesIO(base64.b64decode(base_64_string))
+        img = Image.open(file).resize((28, 28)).convert('L')
+        img_array = np.asarray(img) / 255
+        img_array = img_array.reshape(1, 784)
+        context = {
+            'result': predict(img_array),
+        }
+        return render(self.request, 'mnist/result.html', context)
