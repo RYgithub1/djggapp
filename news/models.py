1158c60a96b816ddf218c52d14d6ab7bcbfa06c6
RYgithub1 <ryo.rec.otani@gmail.com>
2023-04-30 23:17:51 +0200

    Made models

--- news/models.py
+++ news/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Reporter(models.Model):
+    full_name = models.CharField(max_length=77)
+
+    def __str__(self):
+        return self.full_name
+
+
+
+class Article(models.Model):
+    pub_date = models.DateField()
+    headline = models.CharField(max_length=222)
+    content = models.TextField()
+    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.headline
