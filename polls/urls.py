cd3412db5989f28b37e7066e4c93bc1fafdc8521
RYgithub1 <ryo.rec.otani@gmail.com>
2023-05-13 20:55:03 +0200

    Fixed views and urls

--- polls/urls.py
+++ polls/urls.py
@@ -5,4 +5,7 @@ from . import views
 
 urlpatterns = [
     path("", views.pollsindex, name="pollsindex"),
+    path("<int:question_id>/", views.pollsdetail, name="pollsdetail"),
+    path("<int:question_id>/results/", views.pollsresults, name="pollsresults"),
+    path("<int:question_id>/vote/", views.pollsvote, name="pollsvote"),
 ]
