2e18519b57121ae356bdc141456b5cbd42458099
RYgithub1 <ryo.rec.otani@gmail.com>
2023-05-18 21:43:32 +0200

    Fixed detailviews

--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,19 @@
 from django.http import HttpResponse
 from .models import Question
-from django.template import loader
+# from django.template import loader
+from django.shortcuts import render
+from django.http import Http404
 
 
 
 def pollsindex(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/polls_index.html")
+    # template = loader.get_template("polls/polls_index.html")
     context = {
         "latest_question_list": latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, "polls/polls_index.html", context)
+    # return HttpResponse(template.render(context, request))
     # output = ", ".join([q.question_text for q in latest_question_list])
     # return HttpResponse(output)
     # return HttpResponse("Polls index...")
@@ -18,7 +21,12 @@ def pollsindex(request):
 
 
 def pollsdetail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/polls_detail.html", {"question": question})
+    # return HttpResponse("You're looking at question %s." % question_id)
 
 
 
