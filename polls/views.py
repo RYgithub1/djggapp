d37c138a1b3576aea6653e35811501e46941c7de
RYgithub1 <ryo.rec.otani@gmail.com>
2023-05-19 21:58:19 +0200

    Fixed views

--- polls/views.py
+++ polls/views.py
@@ -1,18 +1,20 @@
 from django.http import HttpResponse
 from .models import Question
 # from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 
 
 
-def pollsindex(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+def pollsindex(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/polls_index.html", {"question": question})
+    # latest_question_list = Question.objects.order_by("-pub_date")[:5]
     # template = loader.get_template("polls/polls_index.html")
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return render(request, "polls/polls_index.html", context)
+    # context = {
+    #     "latest_question_list": latest_question_list,
+    # }
+    # return render(request, "polls/polls_index.html", context)
     # return HttpResponse(template.render(context, request))
     # output = ", ".join([q.question_text for q in latest_question_list])
     # return HttpResponse(output)
